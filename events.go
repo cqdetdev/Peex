a77b40973d7250d99485088edcd7a553fdfb136c
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2023-06-27 11:14:40 -0400

    sign edit

--- events.go
+++ events.go
@@ -284,7 +284,7 @@ type eventPunchAirHandler interface {
 }
 
 type eventSignEditHandler interface {
-	HandleSignEdit(ctx *event.Context, oldText, newText string)
+	HandleSignEdit(ctx *event.Context, frontSide bool, oldText, newText string)
 }
 
 type eventItemDamageHandler interface {
@@ -455,9 +455,9 @@ func (s *Session) HandlePunchAir(ctx *event.Context) {
 	})
 }
 
-func (s *Session) HandleSignEdit(ctx *event.Context, oldText, newText string) {
+func (s *Session) HandleSignEdit(ctx *event.Context, frontSide bool, oldText, newText string) {
 	s.handleEvent(eventSignEdit, func(h Handler) {
-		h.(eventSignEditHandler).HandleSignEdit(ctx, oldText, newText)
+		h.(eventSignEditHandler).HandleSignEdit(ctx, frontSide, oldText, newText)
 	})
 }
 
@@ -467,9 +467,9 @@ func (s *Session) HandleItemDamage(ctx *event.Context, i item.Stack, damage int)
 	})
 }
 
-func (s *Session) HandleItemPickup(ctx *event.Context, i item.Stack) {
+func (s *Session) HandleItemPickup(ctx *event.Context, i *item.Stack) {
 	s.handleEvent(eventItemPickup, func(h Handler) {
-		h.(eventItemPickupHandler).HandleItemPickup(ctx, &i)
+		h.(eventItemPickupHandler).HandleItemPickup(ctx, i)
 	})
 }
 
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/df-mc/atomic v1.10.0
-	github.com/df-mc/dragonfly v0.9.5
+	github.com/df-mc/dragonfly v0.9.7
 	github.com/go-gl/mathgl v1.0.0
 	github.com/google/uuid v1.3.0
 )
@@ -13,14 +13,15 @@ require (
 	github.com/brentp/intintmap v0.0.0-20190211203843-30dc0ade9af9 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/df-mc/goleveldb v1.1.9 // indirect
-	github.com/df-mc/worldupgrader v1.0.6 // indirect
+	github.com/df-mc/worldupgrader v1.0.7 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/muhammadmuzzammil1998/jsonc v1.0.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/sandertv/go-raknet v1.12.0 // indirect
-	github.com/sandertv/gophertunnel v1.29.0 // indirect
+	github.com/sandertv/gophertunnel v1.30.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
